952001d66f7febadca0a74960876f1f8ad8efaff
ashesfall <ashesfall@almostrealism.com>
2018-02-22 02:28:42 -0800

    Introduced getRow method.

--- math/org/almostrealism/algebra/Mat3f.java
+++ math/org/almostrealism/algebra/Mat3f.java
@@ -56,8 +56,31 @@ public class Mat3f {
 	}
 
 	/**
-	 * Copies the matrix values in the specified column into the vector
-	 * parameter.
+	 * Copies the matrix values in the specified row into the vector parameter.
+	 *
+	 * @param row  The matrix row
+	 * @param v  The vector into which the matrix row values will be copied
+	 */
+	public final void getRow(int row, Vec3f v) {
+		if (row == 0) {
+			v.setX(get(0, 0));
+			v.setY(get(0, 1));
+			v.setZ(get(0, 2));
+		} else if (row == 1) {
+			v.setX(get(1, 0));
+			v.setY(get(1, 1));
+			v.setZ(get(1, 2));
+		} else if (row == 2) {
+			v.setX(get(2, 0));
+			v.setY(get(2, 1));
+			v.setZ(get(2, 2));
+		} else {
+			throw new ArrayIndexOutOfBoundsException(row);
+		}
+	}
+
+	/**
+	 * Copies the matrix values in the specified column into the vector parameter.
 	 *
 	 * @param column  The matrix column
 	 * @param v  The vector into which the matrix row values will be copied
