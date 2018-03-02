fa52727b4d0de27a44710755317e432daa851239
ashesfall <ashesfall@almostrealism.com>
2018-03-01 22:29:35 -0800

    Added FloatList.

---
+++ math/org/almostrealism/algebra/FloatList.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018 Michael Murray
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.almostrealism.algebra;
+
+/** Growable array of floats. */
+public class FloatList {
+	private static final int DEFAULT_SIZE = 10;
+
+	private float[] data = new float[DEFAULT_SIZE];
+	private int numElements;
+
+	public void add(float f) {
+		if (numElements == data.length) {
+			resize(1 + numElements);
+		}
+		data[numElements++] = f;
+		assert numElements <= data.length;
+	}
+
+	public int size() {
+		return numElements;
+	}
+
+	public float get(int index) {
+		if (index >= numElements) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		return data[index];
+	}
+
+	public void put(int index, float val) {
+		if (index >= numElements) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		data[index] = val;
+	}
+
+	public void trim() {
+		if (data.length > numElements) {
+			float[] newData = new float[numElements];
+			System.arraycopy(data, 0, newData, 0, numElements);
+			data = newData;
+		}
+	}
+
+	public float[] getData() {
+		return data;
+	}
+
+	private void resize(int minCapacity) {
+		int newCapacity = 2 * data.length;
+		if (newCapacity == 0) {
+			newCapacity = DEFAULT_SIZE;
+		}
+		if (newCapacity < minCapacity) {
+			newCapacity = minCapacity;
+		}
+		float[] newData = new float[newCapacity];
+		System.arraycopy(data, 0, newData, 0, data.length);
+		data = newData;
+	}
+}
