fbb5ef66dfeb343f0630bf8218254cc8678b3b0d
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2020-09-03 07:17:01 -0700

    Expanded convenience methods of TriangleDataProducer.

--- src/org/almostrealism/graph/mesh/TriangleDataProducer.java
+++ src/org/almostrealism/graph/mesh/TriangleDataProducer.java
@@ -5,15 +5,33 @@ import org.almostrealism.algebra.VectorProducer;
 import org.almostrealism.util.Producer;
 
 public interface TriangleDataProducer extends Producer<TriangleData> {
-	static TriangleDataFromVectors of(Producer<Vector> p1, Producer<Vector> p2, Producer<Vector> p3) {
-		Producer<Vector> abc = VectorProducer.subtract(p1, p2);
-		Producer<Vector> def = VectorProducer.subtract(p1, p3);
-		Producer<Vector> jkl = p1;
+	default VectorFromTriangleData abc() { return abc(this); }
 
-		VectorProducer a = VectorProducer.subtract(p2, p1);
-		VectorProducer b = VectorProducer.subtract(p3, p1);
-		VectorProducer normal = a.crossProduct(b);
-		normal = normal.normalize();
-		return new TriangleDataFromVectors(abc, def, jkl, normal);
+	static VectorFromTriangleData abc(Producer<TriangleData> t) {
+		return new VectorFromTriangleData(t, VectorFromTriangleData.ABC);
+	}
+
+	default VectorFromTriangleData def() { return def(this); }
+
+	static VectorFromTriangleData def(Producer<TriangleData> t) {
+		return new VectorFromTriangleData(t, VectorFromTriangleData.DEF);
+	}
+
+	default VectorFromTriangleData jkl() { return jkl(this); }
+
+	static VectorFromTriangleData jkl(Producer<TriangleData> t) {
+		return new VectorFromTriangleData(t, VectorFromTriangleData.JKL);
+	}
+
+	default VectorFromTriangleData normal() { return normal(this); }
+
+	static VectorFromTriangleData normal(Producer<TriangleData> t) {
+		return new VectorFromTriangleData(t, VectorFromTriangleData.NORMAL);
+	}
+
+	static TriangleDataFromVectors of(Producer<Vector> p1, Producer<Vector> p2, Producer<Vector> p3) {
+		return new TriangleDataFromVectors(VectorProducer.subtract(p2, p1),
+											VectorProducer.subtract(p3, p1),
+											p1);
 	}
 }
