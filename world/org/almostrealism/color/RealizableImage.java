1bf9780a2b92e1029d2ffce175140d1429c36457
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2020-09-26 18:02:23 -0700

    Simplified RealizableImage so that it just produces an RGB array from a single pixel producer.

--- world/org/almostrealism/color/RealizableImage.java
+++ world/org/almostrealism/color/RealizableImage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Michael Murray
+ * Copyright 2020 Michael Murray
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,122 +16,62 @@
 
 package org.almostrealism.color;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
 import org.almostrealism.algebra.Pair;
-import org.almostrealism.color.computations.ColorProducer;
-import org.almostrealism.util.Pipeline;
+import org.almostrealism.algebra.PairBank;
+import org.almostrealism.hardware.KernelizedProducer;
+import org.almostrealism.hardware.MemoryBank;
 import org.almostrealism.util.Producer;
 
-public class RealizableImage implements Producer<Producer<RGB>[][]> {
-	private Producer<RGB> data[][];
-	private Future<Producer<RGB>> futures[][];
-
-	private Producer<RGB> source;
+public class RealizableImage implements Producer<RGB[][]> {
+	private KernelizedProducer<RGB> source;
 	private Pair dim;
-	
-	public RealizableImage(Producer<RGB> data[][]) { this.data = data; }
-	
-	public RealizableImage(Future<Producer<RGB>> data[][]) { this.futures = data; }
 
-	public RealizableImage(Producer<RGB> source, Pair dimensions) {
+	public RealizableImage(KernelizedProducer<RGB> source, Pair dimensions) {
 		this.source = source;
 		this.dim = dimensions;
 	}
 
-	public Producer<RGB> getSource() { return source; }
+	public KernelizedProducer<RGB> getSource() { return source; }
 
-	public boolean isCompletelySubmitted() {
-		if (futures == null) return true;
+	public Pair getDimensions() { return dim; }
 
-		for (int i = 0; i < futures.length; i++) {
-			for (int j = 0; j < futures[i].length; j++) {
-				if (futures[i][j] == null) return false;
-			}
-		}
-
-		return true;
-	}
-
-	public boolean isComplete() {
-		if (futures == null) return true;
-
-		for (int i = 0; i < futures.length; i++) {
-			for (int j = 0; j < futures[i].length; j++) {
-				if (futures[i][j] == null || !futures[i][j].isDone()) return false;
-			}
-		}
-
-		return true;
-	}
-
-	public double getCompleted() {
-		if (futures == null) return 1.0;
-
-		int completed = 0, total = 0;
-
-		for (int i = 0; i < futures.length; i++) {
-			for (int j = 0; j < futures[i].length; j++) {
-				if (futures[i][j] != null && futures[i][j].isDone()) completed++;
-				total++;
-			}
-		}
-
-		return ((double) completed) / total;
+	@Override
+	public RGB[][] evaluate() {
+		return evaluate(new Object[] { new Pair(0, 0) });
 	}
 
 	@Override
-	public Producer<RGB>[][] evaluate(Object[] args) {
-		Pipeline pipe = null;
-		if (args != null && args.length > 0 && args[0] instanceof Pipeline) pipe = (Pipeline) args[0];
+	public RGB[][] evaluate(Object[] args) {
+		System.out.println("RealizableImage: Evaluating source kernel...");
 
-		if (source == null) {
-			if (this.futures == null) return data;
+		Pair xy = (Pair) args[0];
+		int x = (int) xy.getX();
+		int y = (int) xy.getY();
+		int w = (int) dim.getX();
+		int h = (int) dim.getY();
+		int size = w * h;
+		PairBank input = new PairBank(size);
+		RGBBank output = new RGBBank(size);
 
-			Producer<RGB> p[][] = new Producer[futures.length][futures[0].length];
-			for (int i = 0; i < p.length; i++) {
-				for (int j = 0; j < p[i].length; j++) {
-					try {
-						p[i][j] = (futures[i][j] == null || !futures[i][j].isDone()) ? null : futures[i][j].get();
-					} catch (InterruptedException | ExecutionException e) {
-						e.printStackTrace();
-					}
-				}
-
-				if (pipe != null) pipe.evaluate(new Object[]{ p });
+		for (int i = 0; i < w; i++) {
+			for (int j = 0; j < h; j++) {
+				input.set(j * w + i, x + i, h - 1 - j - y);
 			}
-
-			return p;
-		} else {
-			// TODO  Theres a better way of doing this
-			Producer<RGB> p[][] = new Producer[(int) dim.getX()][(int) dim.getY()];
-
-			for (int i = 0; i < p.length; i++) {
-				for (int j = 0; j < p[i].length; j++) {
-					p[i][j] = source;
-				}
-
-				if (pipe != null) pipe.evaluate(new Object[]{ p });
-			}
-
-			return p;
 		}
+
+		source.kernelEvaluate(output, new MemoryBank[] { input });
+
+		RGB image[][] = new RGB[w][h];
+
+		for (int i = 0; i < w; i++) {
+			for (int j = 0; j < h; j++) {
+				image[i][j] = output.get(j * w + i);
+			}
+		}
+
+		return image;
 	}
 
-	/**
-	 * If this {@link RealizableImage} is backed by actual {@link ColorProducer}s,
-	 * {@link Producer#compact()} is called on each of them. If {@link Future}s
-	 * are being used instead, this method does nothing.
-	 */
 	@Override
-	public void compact() {
-		if (data == null) return;
-
-		for (int i = 0; i < data.length; i++) {
-			for (int j = 0; j < data[i].length; j++) {
-				if (data[i][j] != null) data[i][j].compact();
-			}
-		}
-	}
+	public void compact() { source.compact(); }
 }
