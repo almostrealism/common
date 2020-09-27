0711f35422beab3f34dbb53b37049578893da28e
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2020-09-26 23:01:28 -0700

    Added createKernelDestination implementation.

--- src/org/almostrealism/util/AcceleratedStaticPairProducer.java
+++ src/org/almostrealism/util/AcceleratedStaticPairProducer.java
@@ -1,10 +1,31 @@
+/*
+ * Copyright 2020 Michael Murray
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.almostrealism.util;
 
 import org.almostrealism.algebra.Pair;
+import org.almostrealism.algebra.PairBank;
 import org.almostrealism.algebra.PairProducer;
+import org.almostrealism.hardware.MemoryBank;
 
 public class AcceleratedStaticPairProducer extends AcceleratedStaticProducer<Pair> implements PairProducer {
 	public AcceleratedStaticPairProducer(Pair value, Producer<Pair> output) {
 		super(value, output);
 	}
+
+	@Override
+	public MemoryBank<Pair> createKernelDestination(int size) { return new PairBank(size); }
 }
