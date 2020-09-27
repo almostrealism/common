aba9228e0a7b986d8a102ad646526001a124d21a
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2020-09-26 22:48:12 -0700

    Improvements to ImmutableVector, which is now an AcceleratedStaticVectorProducer.

--- math/org/almostrealism/algebra/ImmutableVector.java
+++ math/org/almostrealism/algebra/ImmutableVector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Michael Murray
+ * Copyright 2020 Michael Murray
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,95 +18,59 @@ package org.almostrealism.algebra;
 
 import io.almostrealism.code.Scope;
 import io.almostrealism.code.Variable;
+import org.almostrealism.hardware.KernelizedProducer;
+import org.almostrealism.hardware.MemWrapper;
+import org.almostrealism.hardware.MemoryBank;
 import org.almostrealism.relation.NameProvider;
 import org.almostrealism.relation.TripleFunction;
+import org.almostrealism.util.AcceleratedStaticVectorProducer;
 import org.almostrealism.util.Producer;
 
-public class ImmutableVector implements VectorProducer, TripleFunction<Vector> {
-	private boolean initialized = false;
-	private Vector value;
-
+public class ImmutableVector extends AcceleratedStaticVectorProducer implements TripleFunction<Vector> {
 	public ImmutableVector() {
-		this.value = new Vector() {
-			protected void setMem(int offset, double[] source, int srcOffset, int length) {
-				if (initialized)
-					throw new RuntimeException("Vector is immutable");
-				else
-					super.setMem(offset, source, srcOffset, length);
-			}
-
-			protected void setMem(int offset, Vector src, int srcOffset, int length) {
-				if (initialized)
-					throw new RuntimeException("Vector is immutable");
-				else
-					super.setMem(offset, src, srcOffset, length);
-			}
-		};
+		super(new Data(), Vector.blank());
 	}
 
 	public ImmutableVector(double x, double y, double z) {
-		this.value = new Vector(x, y, z) {
-			protected void setMem(int offset, double[] source, int srcOffset, int length) {
-				if (initialized)
-					throw new RuntimeException("Vector is immutable");
-				else
-					super.setMem(offset, source, srcOffset, length);
-			}
-
-			protected void setMem(int offset, Vector src, int srcOffset, int length) {
-				if (initialized)
-					throw new RuntimeException("Vector is immutable");
-				else
-					super.setMem(offset, src, srcOffset, length);
-			}
-		};
-
-		initialized = true;
+		super(new Data(), Vector.blank());
+		setValue(new Vector(x, y, z));
 	}
 
 	public ImmutableVector(Vector v) {
-		this.value = new Vector() {
-			protected void setMem(int offset, double[] source, int srcOffset, int length) {
-				if (initialized)
-					throw new RuntimeException("Vector is immutable");
-				else
-					super.setMem(offset, source, srcOffset, length);
-			}
-
-			protected void setMem(int offset, Vector src, int srcOffset, int length) {
-				if (initialized)
-					throw new RuntimeException("Vector is immutable");
-				else
-					super.setMem(offset, src, srcOffset, length);
-			}
-		};
-
-		this.value.setTo(v);
-
-		initialized = true;
+		super(new Data(), Vector.blank());
+		setValue(v);
 	}
 
 	@Override
-	public Vector evaluate(Object[] args) {
-		return value;
-	}
+	public Vector operate(Triple in) { return getValue(); }
 
 	@Override
-	public void compact() {
-
-	}
-
-	@Override
-	public Vector operate(Triple in) {
-		return value;
-	}
-
-	@Override
-	public Scope<? extends Variable> getScope(NameProvider p) {
+	public Scope<Vector> getScope(NameProvider p) {
 		return null;
 	}
 
-	public Vector getValue() { return (Vector) value.clone(); }
+	@Override
+	public Vector getValue() { return (Vector) super.getValue().clone(); }
 
-	public void setValue(Vector v) { value.setTo(v); initialized = true; }
+	public void setValue(Vector v) { super.getValue().setTo(v); ((Data) super.getValue()).initialized = true; }
+
+	private static class Data extends Vector {
+		private boolean initialized;
+
+		@Override
+		public void setMem(int offset, double[] source, int srcOffset, int length) {
+			if (initialized)
+				throw new RuntimeException("Vector is immutable");
+			else
+				super.setMem(offset, source, srcOffset, length);
+		}
+
+		@Override
+		public void setMem(int offset, MemWrapper src, int srcOffset, int length) {
+			if (initialized)
+				throw new RuntimeException("Vector is immutable");
+			else
+				super.setMem(offset, src, srcOffset, length);
+		}
+	}
 }
