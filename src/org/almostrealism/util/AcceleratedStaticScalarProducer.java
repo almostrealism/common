4c0062900c1a917f96317a1beba949522c93ff8e
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2020-09-26 23:01:17 -0700

    Added createKernelDestination implementation.

--- src/org/almostrealism/util/AcceleratedStaticScalarProducer.java
+++ src/org/almostrealism/util/AcceleratedStaticScalarProducer.java
@@ -17,10 +17,15 @@
 package org.almostrealism.util;
 
 import org.almostrealism.algebra.Scalar;
+import org.almostrealism.algebra.ScalarBank;
 import org.almostrealism.algebra.ScalarProducer;
+import org.almostrealism.hardware.MemoryBank;
 
 public class AcceleratedStaticScalarProducer extends AcceleratedStaticProducer<Scalar> implements ScalarProducer {
 	public AcceleratedStaticScalarProducer(Scalar value, Producer<Scalar> output) {
 		super(value, output);
 	}
+
+	@Override
+	public MemoryBank<Scalar> createKernelDestination(int size) { return new ScalarBank(size); }
 }
