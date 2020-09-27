2860a76a832b48f2c2e555b8ed919e18b633242d
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2020-09-27 12:07:43 -0700

    Introduced DynamicRGBProducer and DynamicScalarProducer.

---
+++ src/org/almostrealism/util/DynamicRGBProducer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Michael Murray
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.almostrealism.util;
+
+import org.almostrealism.color.RGB;
+import org.almostrealism.color.RGBBank;
+import org.almostrealism.hardware.KernelizedProducer;
+import org.almostrealism.hardware.MemoryBank;
+
+import java.util.function.Function;
+
+public class DynamicRGBProducer extends DynamicProducer<RGB> implements KernelizedProducer<RGB> {
+
+	public DynamicRGBProducer(Function<Object[], RGB> function) {
+		super(function);
+	}
+
+	@Override
+	public MemoryBank<RGB> createKernelDestination(int size) { return new RGBBank(size); }
+}
---
+++ src/org/almostrealism/util/DynamicScalarProducer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Michael Murray
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.almostrealism.util;
+
+import org.almostrealism.algebra.Scalar;
+import org.almostrealism.algebra.ScalarBank;
+import org.almostrealism.hardware.KernelizedProducer;
+import org.almostrealism.hardware.MemoryBank;
+
+import java.util.function.Function;
+
+public class DynamicScalarProducer extends DynamicProducer<Scalar> implements KernelizedProducer<Scalar> {
+
+	public DynamicScalarProducer(Function<Object[], Scalar> function) {
+		super(function);
+	}
+
+	@Override
+	public MemoryBank<Scalar> createKernelDestination(int size) { return new ScalarBank(size); }
+}
