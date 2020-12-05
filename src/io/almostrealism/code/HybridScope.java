99f39d409995d0ca4c16e8b1b9057776ae6aea67
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2020-12-04 21:13:39 -0800

    Introduced HybridScope, which allows the normal features of Scope to be used along with explicit code in the form of an inner ExplicitScope.

---
+++ src/io/almostrealism/code/HybridScope.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Michael Murray
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.almostrealism.code;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class HybridScope<T> extends Scope<T> {
+	private ExplicitScope<T> explicit;
+
+	public HybridScope(OperationAdapter operation) {
+		super(operation.getFunctionName());
+		this.explicit = new ExplicitScope<T>(operation);
+	}
+
+	public HybridScope(ExplicitScope<T> explicit) {
+		this.explicit = explicit;
+	}
+
+	@Override
+	public void write(CodePrintWriter w) {
+		super.write(w);
+		explicit.write(w);
+	}
+
+	public Consumer<String> code() { return explicit.code(); }
+
+	@Override
+	public List<Argument<?>> getArguments() {
+		List<Argument<?>> args = new ArrayList<>();
+		args.addAll(super.getArguments());
+		args.addAll(explicit.getArguments());
+		return removeDuplicateArguments(args);
+	}
+}
