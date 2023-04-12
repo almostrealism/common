0ce1429ebbdb4411841010f39f2889f11cacf08d
Michael Murray <michael@almostrealism.com>
2023-04-11 18:40:56 -0700

    Introduced kotlin and groovy versions of the convolution model definition, as a way of demoing how ML support might be leveraged in practice.

---
+++ ml/src/main/java/org/almostrealism/ml/ModelDemo.kt
@@ -0,0 +1,37 @@
+package org.almostrealism.ml
+
+import org.almostrealism.CodeFeatures
+import org.almostrealism.algebra.Tensor
+import org.almostrealism.collect.TraversalPolicy
+import org.almostrealism.model.Model
+
+fun main() {
+    ModelDemo().train()
+}
+
+class ModelDemo : CodeFeatures {
+    fun model(inputShape: TraversalPolicy?): Model {
+        val model = Model(inputShape)
+        model.addLayer(convolution2d(3, 8))
+        model.addLayer(pool2d(2))
+        model.addBlock(flatten())
+        model.addLayer(dense(10))
+        model.addLayer(softmax())
+        return model
+    }
+
+    fun train() {
+        val shape = shape(100, 100)
+        val model = model(shape)
+        val t = loadImage(shape)
+        val input = t.pack()
+        model.setup().get().run()
+        model.forward(input)
+    }
+
+    fun loadImage(shape: TraversalPolicy): Tensor<Double> {
+        val t = Tensor<Double>()
+        shape.stream().forEach { pos: IntArray -> t.insert(Math.random(), *pos) }
+        return t
+    }
+}
