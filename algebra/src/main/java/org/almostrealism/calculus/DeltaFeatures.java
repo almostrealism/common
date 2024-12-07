b922bd4779752e4509cecf22f419e3031846ea20
Michael Murray <michael@almostrealism.com>
2024-12-07 14:36:32 -0800

    Removed debugging.

--- algebra/src/main/java/org/almostrealism/calculus/DeltaFeatures.java
+++ algebra/src/main/java/org/almostrealism/calculus/DeltaFeatures.java
@@ -99,10 +99,6 @@ public interface DeltaFeatures extends MatrixFeatures {
 			Optional<Producer<T>> match = matchInput(producer, target);
 
 			if (match == null) {
-				if (!Named.nameOf(producer).equals("add") && !Named.nameOf(producer).equals("multiply")) {
-					System.out.println("!");
-				}
-
 				return null;
 			} else if (match.isEmpty()) {
 				return (CollectionProducer<T>) zeros(shape.append(targetShape));
