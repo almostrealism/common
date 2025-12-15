06049f8eedad47660d7bc3418ea874ae5a529352
Michael Murray <michael@almostrealism.com>
2025-12-15 09:40:08 -0800

    Added getters in DelegatedAudioLine.

--- audio/src/main/java/org/almostrealism/audio/line/DelegatedAudioLine.java
+++ audio/src/main/java/org/almostrealism/audio/line/DelegatedAudioLine.java
@@ -51,8 +51,16 @@ public class DelegatedAudioLine implements AudioLine, Delegated<AudioLine> {
 		updateDelegateSettings();
 	}
 
+	public InputLine getInputDelegate() {
+		return inputDelegate;
+	}
+
 	public void setInputDelegate(InputLine delegate) {
 		this.inputDelegate = delegate;
+	}
+
+	public OutputLine getOutputDelegate() {
+		return outputDelegate;
 	}
 
 	public void setOutputDelegate(OutputLine delegate) {
